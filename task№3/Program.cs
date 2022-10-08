ad2d8ee24285cfb446b6346bd89598dcbb3a86ae
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-08 22:56:12 +0300

    Задать номер четверти, показать диапазоны для возможных координат

---
+++ task№3/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+Console.WriteLine("Введите номер четверти:");
+string number = Console.ReadLine() ?? "0";
+
+if (number.ToLower() == "1")
+{
+    Console.WriteLine("диапозоном для возможных координат в 1 четверти является x > 0 y > 0");
+}
+
+else if (number.ToLower() == "2")
+{
+    Console.WriteLine("диапозоном для возможных координат в 2 четверти является x < 0 y > 0");
+}
+
+else if (number.ToLower() == "3")
+{
+    Console.WriteLine("диапозоном для возможных координат в 3 четверти является x < 0 y < 0");
+}
+
+else if (number.ToLower() == "4")
+{
+    Console.WriteLine("диапозоном для возможных координат в 4 четверти является x > 0 y < 0");
+}
+
+else
+{
+     Console.WriteLine("такой четверти не существует");
+}
