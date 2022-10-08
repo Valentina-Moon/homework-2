eaba62618e881a1ae124c19410828747779ff3fe
Valentina-Moon <valentina.tirtik_777@mail.ru>
2022-10-08 23:03:34 +0300

    2D

---
+++ task№4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти расстояние между точками в пространстве 2D/3D
+//2d
+Console.WriteLine("Введите координаты Xa: ");
+double Xa = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты Xb: ");
+double Xb = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Ya: ");
+double Ya = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты Yb: ");
+double Yb = Convert.ToDouble(Console.ReadLine());
+
+double D = Math.Sqrt(Math.Pow(Xb - Xa, 2) + Math.Pow(Yb - Ya, 2));
+
+Console.WriteLine($"Расстояние между двумя точками на плоскости равно {D}");
